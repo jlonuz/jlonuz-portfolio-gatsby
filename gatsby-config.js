c728cfa17bad1db40f58830f66894e627073a02c
jlonuz <lonuz.j@gmail.com>
2023-11-10 17:11:26 -0300

    Finished Gatsby Tutorial Part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,16 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+const adapter = require("gatsby-adapter-netlify")
+
 module.exports = {
   siteMetadata: {
     title: `jlonuz-portfolio`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
+  adapter: adapter({
+    excludeDatastoreFromEngineFunction: false,
+  }),
 }
